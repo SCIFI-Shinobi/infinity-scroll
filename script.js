dcbc2ab3159eacffa90128ba51a1e823623a5a8a
Eyobel Zeleke <eyobelzeleke@gmail.com>
2023-12-08 03:19:09 -0800

    remove placeholders for image and then dynamically add images to page using javascript

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+const imageContainer=document.getElementById('image-container');
+const loader=document.getElementById('loader');
+
+let photoArray=[];
 // unsplash API
 const apiKey='g4OH7Y946zvpwB8On93oGkzriUeLe2YYxnUm35wa_Gg';
 const count=10;
-const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function displayPhoto(){
+   photoArray.forEach(photo=>{
+//    create <a> to link to unsplash
+   const item = document.createElement('a');
+   item.setAttribute('src',photo.links.html);
+   item.setAttribute('target','_blank');
+//    create <img> for photo
+    const img=document.createElement('img');
+    img.setAttribute('href',photo.urls.regular);
+    img.setAttribute('alt',photo.alt_description);
+    img.setAttribute('title',photo.alt_description);
+// put <img> inside <a>,then both inside the image container
+   item.appendChild(img);
+   imageContainer.appendChild(item);
+   
+   });
+}
 
 // get photos from unsplash API
 async function getPhotos(){
     try{
         const response = await fetch(apiUrl);
-        const data= await response.json();
-        console.log(data);
+        photoArray = await response.json();
+        displayPhoto();
     }catch(error){
         // catch error 
     }
