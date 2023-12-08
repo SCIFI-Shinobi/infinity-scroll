c201c07bdc9b8a7fa7b9cf9a1af83fa6a9863a20
Eyobel Zeleke <eyobelzeleke@gmail.com>
2023-12-08 03:33:05 -0800

    implemented helper function for setting attributes

--- script.js
+++ script.js
@@ -7,17 +7,27 @@ const apiKey='g4OH7Y946zvpwB8On93oGkzriUeLe2YYxnUm35wa_Gg';
 const count=10;
 const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// helper function to set attributes
+function setAttributes(element,attributes){
+    for(const key in attributes)
+        element.setAttribute(key,attributes[key]);
+}
+
 function displayPhoto(){
    photoArray.forEach(photo=>{
 //    create <a> to link to unsplash
    const item = document.createElement('a');
-   item.setAttribute('src',photo.links.html);
-   item.setAttribute('target','_blank');
+   setAttributes(item,{
+    src:photo.links.html,
+    target:'_blank'
+   });
 //    create <img> for photo
     const img=document.createElement('img');
-    img.setAttribute('href',photo.urls.regular);
-    img.setAttribute('alt',photo.alt_description);
-    img.setAttribute('title',photo.alt_description);
+        setAttributes(img,{
+        href:photo.urls.regular,
+        alt:photo.alt_description,
+        title:photo.alt_description
+    }); 
 // put <img> inside <a>,then both inside the image container
    item.appendChild(img);
    imageContainer.appendChild(item);
